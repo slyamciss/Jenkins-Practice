99c1591d9b3d5ac3827bc47f912e72bba3d5e2d7
slyamciss <slyamciss@gmail.com>
2022-08-22 19:17:48 -0400

    Third commit

--- project.sh
+++ project.sh
@@ -1,2 +1,3 @@
 echo "This is my Jenkins fullcourse Tutorial. Testing is completed."
 echo "Another line added to the end of the description"
+echo "Another another line added to the end of the description"
