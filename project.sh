ee65f2fa371d1b9f9cb3e59a6f8f6dde1995dbf2
slyamciss <slyamciss@gmail.com>
2022-08-22 19:21:05 -0400

    fourth commit

--- project.sh
+++ project.sh
@@ -1,3 +1,4 @@
 echo "This is my Jenkins fullcourse Tutorial. Testing is completed."
 echo "Another line added to the end of the description"
 echo "Another another line added to the end of the description"
+echo "fourth another line added to the end of the description"
